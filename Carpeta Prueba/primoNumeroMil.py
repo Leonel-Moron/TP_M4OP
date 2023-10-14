b5a0c3e9471dad9845aa3c51d1dcda98a6cade29
Leonel <moron.leonel@gmail.com>
2023-10-14 18:53:13 -0300

    practicando el primo mil

--- Carpeta Prueba/primoNumeroMil.py
+++ Carpeta Prueba/primoNumeroMil.py
@@ -1,8 +1,23 @@
 #Vamos a buscar el numero primo mil
 
-print("hola")
+#Busquemos un iterador primero
+#El final del iterador va a ser primoNumeroMil = 1000
+#Seria un contador de corte el primo mil
 
-cont = [2,2,3,4,1,2,"leonel"]
 
-for i in cont:
-    print(i)
+#Como calcular un primo???
+#Un numero que se divide por 1 por si mismo
+
+iterador = 100
+cont = 0
+listaPares = []
+
+for i in range(iterador):
+    if i % 2 == 0:
+        listaPares.append(i)
+        cont +=1
+        if cont == 10:
+            break
+print(listaPares)
+print(listaPares[-1])
+    
