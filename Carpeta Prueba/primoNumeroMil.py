f1c0934da321535a2c4412ed7341eb8226affa2c
Leonel <moron.leonel@gmail.com>
2023-10-16 11:14:35 -0300

    trabajando con primoMil

--- Carpeta Prueba/primoNumeroMil.py
+++ Carpeta Prueba/primoNumeroMil.py
@@ -7,11 +7,14 @@
 
 #Como calcular un primo???
 #Un numero que se divide por 1 por si mismo
+#Podemos usar el %2 como idea
+#Para limpiar codigo podemos poner que chequee si es primo en una funcion
 
 iterador = 100
 cont = 0
 listaPares = []
 
+#Con for podemos iterar si ya sabemos cuanto queremos iterar de antemano
 for i in range(iterador):
     if i % 2 == 0 and i>0:
         listaPares.append(i)
@@ -20,4 +23,24 @@ for i in range(iterador):
             break
 print(listaPares)
 print(listaPares[-1])
+
+
+numeroSumando = 0
+contadorWhile = 0
+ListaWhile = []
+
+
+#Con while podemos hacerlo infinito sin saber cuanto tenemos q iterar de antemano
+#Falta pasar a codigo para verificar si es primo un nro
+while(True):
+    if numeroSumando %2 == 0 and numeroSumando>0:
+        ListaWhile.append(numeroSumando)
+        contadorWhile +=1
+    if contadorWhile == 15:
+        break
+    numeroSumando +=1
+
+print(ListaWhile)
+print("El numero par numero ", contadorWhile, " es: ", ListaWhile[-1])
+
     
