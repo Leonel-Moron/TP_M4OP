9b3b3c0b22a9d40325343953cda226854f570ef4
Leonel <moron.leonel@gmail.com>
2023-10-17 12:01:25 -0300

    actualizado el primo mil

--- Carpeta Prueba/primoNumeroMil.py
+++ Carpeta Prueba/primoNumeroMil.py
@@ -1,3 +1,22 @@
 #Vamos a buscar el numero primo mil
 
-print("hola")
+#acá lo que quiero hacer es definir un función que me diga si los numero son primos o no 
+def primo (numero):
+    if numero <= 1:
+        return False
+    #primero le digo que el 1 no 
+    for i in range (2, int ()): #la función range la pensé poque podes genera un secuencia, pero si tiene otra idea mejor 
+        if (numero%i==0) and (numero%1==0):
+            return True 
+        #y acá es donde se me rompe, porque no logro decirle que me determine si es par o no
+
+#pienso la segunda funcion que llame a la funcion anterior y que el contador corte cuando encuentre el numero 1000
+def primo_numero_mil ():
+    contador=0
+    while contador <1000: 
+        if primo (numero):
+            contador += 1
+            if contador==1000:
+                return #?? 
+resultado = primo_numero_mil ()
+print (resultado)
