26953ef5b8165497176e3ca57ad928c430293cba
Leonel <moron.leonel@gmail.com>
2023-10-16 21:59:44 -0300

    codigo actualizado

--- Carpeta Prueba/primoNumeroMil.py
+++ Carpeta Prueba/primoNumeroMil.py
@@ -1,5 +1,4 @@
 import time
-inicio = time.time()
 #Vamos a buscar el numero primo mil
 
 #Busquemos un iterador primero
@@ -11,7 +10,7 @@ inicio = time.time()
 #Un numero que se divide por 1 por si mismo, o sea tiene 2 divisores que tienen resto = 0
 #Podemos usar el %2 como idea
 #Para limpiar codigo podemos poner que chequee si es primo en una funcion
-
+"""""
 iterador = 100
 cont = 0
 listaPares = []
@@ -25,32 +24,26 @@ for i in range(iterador):
             break
 print(listaPares)
 print(listaPares[-1])
-
+"""
 
 
 def esPrimo(num):
     cont = 0
-    for i in range(1,num+1):
-        if num%i == 0:
+    for i in range(1,num+1): # Iteramos desde 1 hasta el numero que pasamos
+        if num%i == 0:       # Dividimos el numero por todos sus previos, si el resto es 0 sumamos al contador 1  
             cont +=1
-    return True if cont == 2 or num == 1 else False #Operador ternario
-    """Es lo mismo que poner
-    if cont == 2 or num == 1:
-        return True
-    else:
-        return False
+    """"        
+    La idea es que el primo es divisible por 1 y por si, 
+    si contador > 2 significa que no es primo xq mas de 2 numeros lo dividieron con resto 0
+    Sumamos la condicion num == 1 porque 1 es primo pero no puede cumplir la condicion contador == 2 
     """
-ListaPrimo=[]    
-iter = 887
-for i in range(iter+1):
-    if esPrimo(i):
-        ListaPrimo.append(i)
-print("es el primo numero ", len(ListaPrimo), " el ", ListaPrimo[-1])
-#Con while podemos hacerlo infinito sin saber cuanto tenemos q iterar de antemano
-#Falta pasar a codigo para verificar si es primo un nro
+    if cont == 2 or num == 1:
+        return True  #Si es primo retornamos un TRUE
+    else:
+        return False #Si no es primo retornamos un FALSE
+    # return True if cont == 2 or num == 1 else False #Operador ternario
 
-#hola bart
-
+"""""
 numeroSumando = 0
 contadorWhile = 0
 ListaWhile = []
@@ -61,12 +54,30 @@ while(True):
     if contadorWhile == 1432:
         break
     numeroSumando +=1
-
-
 print("El numero par numero ", contadorWhile, " es: ", ListaWhile[-1])
+"""
 
-fin =time.time()
 
 
-print(fin - inicio)
+
+listaPrimo=[]    
+aumentador = 1
+contadorMil = 0
+
+inicio = time.time()
+
+while(True):                                #True loopea hasta que cortemos con un break
+    if esPrimo(aumentador):                 # Llamamos a la funcion para chequear si es primo
+        listaPrimo.append(aumentador)       # Si la funcion nos devuelve un TRUE, guardamos el numero en una Lista
+        contadorMil += 1                    # Y ademas aumentamos +1 el contador 
+    if contadorMil == 1000:                 # Cuando el contador llegue a 1000 cortamos
+        break                               # Cortamos el loop con un break, el contador ya tiene el nro primo MIL
+    aumentador +=1                          # Aumentador para pasar a la funcion un numero cada vez mas grande
+
+
+print("El primo numero mil es: ", listaPrimo[-1])
+
     
+fin = time.time()
+
+print("Tardamos para encontrarlo: ",fin - inicio)
