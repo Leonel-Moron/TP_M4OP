4a7b3a0a74b97f4cc37da01b9c10a2f7e4127f17
Leonel <moron.leonel@gmail.com>
2023-10-21 22:42:40 -0300

    Version final menos de 4 seg

--- Carpeta Prueba/primoNumeroMil.py
+++ Carpeta Prueba/primoNumeroMil.py
@@ -10,22 +10,22 @@ import time #libreria para medir tiempo de ejecución
 #Un numero que se divide por 1 por si mismo, o sea tiene 2 divisores que tienen resto = 0
 #Podemos usar el %2 como idea
 #Para limpiar codigo podemos poner que chequee si es primo en una funcion
+""""        
+    La idea es que el primo es divisible por 1 y por si, 
+    si contador > 2 significa que no es primo xq mas de 2 numeros lo dividieron con resto 0
+    1 no es primo asi q para q sea primo es cont == 2
+"""
 
-def esPrimo(num):
+def esPrimo(num):               # Funcion para validar si el nro que reciba es Primo
     cont = 0
     for i in range(1,num+1):    # Iteramos desde 1 hasta el numero que pasamos
         if num%i == 0:          # Dividimos el numero por todos sus previos, si el resto es 0 sumamos al contador 1  
             cont +=1
-    """"        
-    La idea es que el primo es divisible por 1 y por si, 
-    si contador > 2 significa que no es primo xq mas de 2 numeros lo dividieron con resto 0
-    1 no es primo asi q para q sea primo es cont == 2
-    """
     if cont == 2:                
         return True             #Si es primo retornamos un TRUE
     else:
         return False            #Si no es primo retornamos un FALSE
-                                #return True if cont == 2 or num == 1 else False        #Operador ternario
+                                #return True if cont == 2 or num == 1 else False        (Operador ternario)
 
 def buscarPrimoMil():
     listaPrimo = []
@@ -38,12 +38,13 @@ def buscarPrimoMil():
         if contadorMil == 1000:                 # Cuando el contador llegue a 1000 cortamos, Podriamos escribir la condicion de corte en el While
             break                               # Cortamos el loop con un break, el contador ya tiene el nro primo MIL
         aumentador +=1                          # Aumentador para pasar a la funcion un numero cada vez mas grande
-    return listaPrimo[-1]
-
+    return listaPrimo[-1]                       # Devolvemos el ultimo primo q guardamos, el numero mil
 
 
 inicio = time.time()
+
 resultado = buscarPrimoMil()
 print("El primo numero mil es: ", resultado)
+
 fin = time.time()
 print("Tardamos para encontrarlo: ", fin - inicio)
