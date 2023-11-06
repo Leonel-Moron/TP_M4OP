e8709bfe78fb0895cb2dbd4bcc5193d87219cfb7
Leonel <moron.leonel@gmail.com>
2023-11-05 23:33:55 -0300

    agregada explicación

--- Carpeta Prueba/PrimoMilTkinterNuevo.py
+++ Carpeta Prueba/PrimoMilTkinterNuevo.py
@@ -2,15 +2,31 @@ import time
 import tkinter as tk
 import pygame as pg
 
+#Vamos a buscar el numero primo mil
+
+#Busquemos un iterador primero
+#El final del iterador va a ser el numero ingresado por consola // primo mil
+#Seria un contador de corte el input
+
+#¿¿Como calcular un primo???
+#Un numero que se divide por 1 y por si mismo, o sea tiene 2 divisores que tienen resto = 0
+#Podemos usar el %2 como idea
+#Para limpiar codigo podemos poner que chequee si es primo en una funcion
+""""
+    La idea es que el primo es divisible por 1 y por si mismo, entonces tiene q haber una bandera q active cuando
+    se divida. Y si es resto = 0, se incrementa la bandera.
+    Si contador/bandera > 2 significa que no es primo xq mas de 2 numeros lo dividieron con resto 0
+    1 no es primo asi q para q sea primo es cont/bandera == 2
+"""
 
 
 def esPrimo(num):               # Funcion para validar si el nro que reciba es Primo
-    cont = 0
+    cont = 0                    # Inicializamos un contador
     for i in range(1,num+1):    # Iteramos desde 1 hasta el numero que pasamos
         if num%i == 0:          # Dividimos el numero por todos sus previos, si el resto es 0 sumamos al contador 1  
             cont +=1
     if cont == 2:                
-        return True             #Si es primo retornamos un TRUE
+        return True             #Si es primo retornamos un TRUE 
     else:
         return False            #Si no es primo retornamos un FALSE
                                 #return True if cont == 2 or num == 1 else False        (Operador ternario)
